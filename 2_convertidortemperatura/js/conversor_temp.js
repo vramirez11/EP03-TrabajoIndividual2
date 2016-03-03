f95db2f597d51abe48b36545805899990f553877
Vannessa Ramirez Rubio <vannedark@hotmail.com>
2016-03-03 16:14:54 -0500

    EP03 - TrabajoIndividual2

---
+++ 2_convertidortemperatura/js/conversor_temp.js
@@ -0,0 +1,25 @@
+window.onload = function()
+{
+	
+    	alert("Grados Centigrados a Grados Farenheit");
+
+		var temp;
+		temp=prompt("Escriba Grados Centigrados");
+		var hTemp = ((temp * 9) / 5) + 32;
+		alert("Grados Farenheit: " + hTemp);
+
+		alert("Grados Centigrados a Grados Kelvin");
+
+		var tempc;
+		
+		tempc=prompt("Escriba Grados Centigrados");
+		var tempk = ((tempc * 1)) + 273;
+		alert("Grados Kelvin: " + tempk);
+
+		
+
+
+		
+		
+
+};
---
+++ 2_convertidortemperatura/js/kelvin.js
@@ -0,0 +1,11 @@
+window.onload = function()
+{
+
+		alert("Grados Centigrados a Grados Kelvin");
+
+		var tempc;
+		
+		tempc=prompt("Escriba Grados Centigrados");
+		var tempk = tempc + 273;
+		alert("Grados Kelvin: " + tempk);
+	}
